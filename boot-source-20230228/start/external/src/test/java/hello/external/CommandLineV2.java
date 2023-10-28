a607edea5404923f5b02bc4b7341d51f98c3d9e2
kimhyunso <gustn9515@gmail.com>
2023-10-28 18:17:10 +0900

    20231028 18:17

---
+++ boot-source-20230228/start/external/src/test/java/hello/external/CommandLineV2.java
@@ -0,0 +1,45 @@
+package hello.external;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
+
+import java.util.List;
+import java.util.Set;
+
+@Slf4j
+public class CommandLineV2 {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            log.info("arg {}", arg);
+        }
+
+
+        ApplicationArguments appArguments = new DefaultApplicationArguments(args);
+
+        log.info("SourceArgs={}", List.of(appArguments.getSourceArgs()));
+        log.info("NonOptionArgs = {}", appArguments.getNonOptionArgs());
+        log.info("OptionsNames = {}", appArguments.getOptionNames());
+
+        Set<String> optionNames = appArguments.getOptionNames();
+
+        for (String optionName : optionNames) {
+            log.info("option arg {}={}", optionName, appArguments.getOptionValues(optionName));
+
+        }
+
+
+        List<String> url = appArguments.getOptionValues("url");
+        List<String> username = appArguments.getOptionValues( "username");
+        List<String> password = appArguments.getOptionValues("password");
+
+        List<String> mode = appArguments.getOptionValues("mode");
+
+        log.info("url {}",url);
+        log.info("username {}",username);
+        log.info("password {}",password);
+        log.info("mode {}",mode);
+
+
+    }
+}
---
+++ boot-source-20230228/start/external/src/test/java/hello/external/JavaSystemProperties.java
@@ -0,0 +1,33 @@
+package hello.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Properties;
+
+@Slf4j
+public class JavaSystemProperties {
+
+    public static void main(String[] args) {
+
+        System.setProperty("helloKey!!", "helloValue!!");
+        String property = System.getProperty("helloKey!!");
+        log.info("helloKey={}", property);
+
+
+        Properties properties = System.getProperties();
+        for (Object key : properties.keySet()){
+            log.info("prop {}={}", key, System.getProperty(String.valueOf(key)));
+        }
+
+
+        String url = System.getProperty("url");
+        String username = System.getProperty("username");
+        String password = System.getProperty("password");
+
+        log.info("url={}", url);
+        log.info("username={}", username);
+        log.info("password={}", password);
+
+
+    }
+}
